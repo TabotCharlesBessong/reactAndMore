9c7d354a5c2dee9605f841304f0f89aa0eaa1fd6
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-02-12 07:34:21 +0100

    code refactoring with formik components

--- formik/src/App.js
+++ formik/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import OldYoutubeForm from './components/OldYoutubeForm';
+import YoutubeForm from './components/YoutubeForm';
 
 function App() {
   return (
     <div className="App">
-      <OldYoutubeForm/>
+      {/* <OldYoutubeForm/> */}
+      <YoutubeForm/>
     </div>
   );
 }
---
+++ formik/src/components/YoutubeForm.js
@@ -0,0 +1,66 @@
+
+import React from 'react'
+import { Formik , Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+
+const initialValues = {
+  name: '',
+  email: '',
+  channel: ''
+}
+
+const onSubmit = values => {
+  console.log('Form data', values)
+}
+
+
+const validationSchema = Yup.object({
+  name: Yup.string().required('Required'),
+  email: Yup.string()
+    .email('Invalid email format')
+    .required('Required'),
+  channel: Yup.string().required('Required')
+})
+
+function YoutubeForm () {
+
+  return (
+    <Formik 
+    initialValues={initialValues}
+    validationSchema={validationSchema}
+    onSubmit={onSubmit}
+     >
+
+      <Form >
+        <div className='form-control'>
+          <label htmlFor='name'>Name</label>
+          <Field
+            type='text'  id='name'    name='name'
+          />
+         <ErrorMessage name='name' />
+        </div>
+  
+        <div className='form-control'>
+          <label htmlFor='email'>E-mail</label>
+          <Field
+            type='email'      id='email'     name='email'
+          />
+        <ErrorMessage name='email' />
+        </div>
+  
+        <div className='form-control'>
+          <label htmlFor='channel'>Channel</label>
+          <Field
+            type='text'      id='channel'           name='channel'
+          />
+        <ErrorMessage name='channel' />
+        </div>
+  
+        <button type='submit'>Submit</button>
+      </Form>
+    </Formik>
+
+  )
+}
+
+export default YoutubeForm
