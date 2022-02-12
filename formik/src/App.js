9dfe48660cecf23424b05d593a5994d5a568bd66
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-02-12 07:05:51 +0100

    form validation with formik and yup

--- formik/src/App.js
+++ formik/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import OldYoutubeForm from './components/OldYoutubeForm';
 
 function App() {
   return (
     <div className="App">
-       <h1>Hello world</h1>
+      <OldYoutubeForm/>
     </div>
   );
 }
