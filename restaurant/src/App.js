e2f186b7c8c84f6833bdde24e4f8ec527c2f0fc3
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-02-15 10:13:27 +0100

    done with the header components

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 // import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import  Navbar  from './components/Navbar/Navbar';
 import './App.css';
+import Header from './container/Header/Header';
 
 const App = () => (
   <div>
   {/* <h1>Hello world</h1> */}
     <Navbar />
-
+    {/* <Header /> */}
+    <Header/>
+    
   </div>
 );
 
---
+++ restaurant/src/container/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import SubHeading from '../../components/SubHeading/SubHeading';
+import images from '../../constants/images'
+
+import './Header.css';
+
+const Header = () => (
+  <div className='app__header app__wrapper section__padding' id='home' >
+    <div className="app__wrapper_info">
+       <SubHeading
+         title="Chase The New Flavour"
+       />
+       <h1 className='app__header-h1' >
+         The key to find dinnig
+       </h1>
+       <p className="p__opensans" style={{margin:'2rem 0'}} >
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt autem quibusdam libero id quia accusamus!
+       </p>
+       <button type='button' className='custom__button' ></button>
+    </div>
+    <div className="app__wrapper_img">
+       <img src={images.welcome} alt="header-img" />
+    </div>
+  </div>
+);
+
+export default Header;
