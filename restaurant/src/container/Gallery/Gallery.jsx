e3477f8efa838fd389982fec87a99f2a910abbba
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-02-18 13:24:06 +0100

    Gallery complete

--- restaurant/src/container/Gallery/Gallery.jsx
+++ restaurant/src/container/Gallery/Gallery.jsx
@@ -1,58 +1,48 @@
-import React, { useRef } from 'react';
-import SubHeading from '../../components/SubHeading/SubHeading';
-import images from '../../constants/images';
-import { BsInstagram , BsArrowLeftShort , BsArrowRightShort } from 'react-icons/bs';
+import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
+// import SubHeading  from '../../components';
+import SubHeading from '../../components/SubHeading/SubHeading'
+import  images  from '../../constants/images';
 import './Gallery.css';
 
 const Gallery = () => {
-  const scrollRef = useRef(null)
-  
-  const scroll = (dir)=> {
-    const {curr} = scrollRef
+  const scrollRef = React.useRef(null);
 
-    if(dir === 'left'){
-      curr.scrollLeft -= 300
-    }else{
-      curr.scrollLeft +=300
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
     }
-  }
+  };
 
-  const image = [
-    images.gallery01,images.gallery02,images.gallery03,images.gallery04
-  ]
-  return(
-    <div className='app__gallery flex__center' >
+  return (
+    <div className="app__gallery flex__center">
       <div className="app__gallery-content">
-        <SubHeading title='Instagram' />
-        <h1 className="headtext__cormorant">
-          Photo Gallery
-        </h1>
-        <p className="p__opensans" style={{color:'#aaa',marginTop:'2rem'}} >
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, fugit maxime suscipit vero dolore dignissimos.
-        </p>
-        <button type='button' className='custom__button' >
-          view more
-        </button>
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button type="button" className="custom__button">View More</button>
       </div>
       <div className="app__gallery-images">
-        <div className="app__gallery-images_container" ref={scrollRef} >
-         {
-          image.map((img,index)=>(
-            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index+1}`} >
-             <img src={img} alt="gallery" />
-             <BsInstagram className='gallery__image-icon' />
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
+            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery_image" />
+              <BsInstagram className="gallery__image-icon" />
             </div>
-          ))
-         }
+          ))}
         </div>
         <div className="app__gallery-images_arrows">
-          <BsArrowLeftShort className='gallery__arrow-icon' onClick={()=> scroll('left') } />
-          <BsArrowRightShort className='gallery__arrow-icon' onClick={()=> scroll('right') } />
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Gallery;
