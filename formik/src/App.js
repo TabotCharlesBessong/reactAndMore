8a0d7b13f9c9236636af1342535cce664a845340
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-02-18 12:27:01 +0100

    making some changes to our form

--- formik/src/App.js
+++ formik/src/App.js
@@ -4,7 +4,7 @@ import LoginForm from './components/LoginForm';
 import {Grid} from '@material-ui/core'
 import './styles.css'
 import RegistrationForm from './components/RegistrationForm';
-// import YoutubeForm from './components/YoutubeForm';
+import YoutubeForm from './components/YoutubeForm';
 
 function App() {
   return (
@@ -12,14 +12,14 @@ function App() {
       {/* <OldYoutubeForm/> */}
       {/* <YoutubeForm/> */}
       <FormikContainer/>
-      {/* <Grid container> */}
-        {/* <Grid item sm={12} md={8} lg={6}> */}
-            {/* <LoginForm/>    */}
-        {/* </Grid> */}
-        {/* <Grid item sm={12} md={8} lg={6}> */}
-             {/* <RegistrationForm/>    */}
-         {/* </Grid> */}
-      {/* </Grid> */}
+      <Grid container>
+        <Grid item sm={12} md={8} lg={6}>
+            <LoginForm/>   
+        </Grid>
+        <Grid item sm={12} md={8} lg={6}>
+             <RegistrationForm/>   
+         </Grid>
+      </Grid>
     </div>
   );
 }
--- formik/src/components/RegistrationForm.js
+++ formik/src/components/RegistrationForm.js
@@ -1,79 +1,85 @@
-
-import { Form, Formik, validateYupSchema } from 'formik'
 import React from 'react'
+import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
 import FormikControl from './FormikControl'
 
-const RegistrationForm = () => {
-
+function RegistrationForm () {
   const options = [
-    {key:'Email',value:'emailloc'},
-    {key:'Telephone',value:'telephonemoc'}
+    { key: 'Email', value: 'emailmoc' },
+    { key: 'Telephone', value: 'telephonemoc' }
   ]
-
   const initialValues = {
-    email:'',
-    password:'',
-    confirmPassword:'',
-    modeOfContact:'',
-    phone:''
+    email: '',
+    password: '',
+    confirmPassword: '',
+    modeOfContact: '',
+    phone: ''
   }
 
   const validationSchema = Yup.object({
-    email:Yup.string().email('Invalid email format').required('Required field'),
-    password:Yup.string().required('Required field'),
-    confirmPassword:Yup.string().oneOf([Yup.ref('password'),''],'Passwords must match'),
-    modeOfContact:Yup.string().required('Required'),
-    phone:Yup.string().when('modeOfContact',{
-      is:'telephonemoc',
-      then:Yup.string().required('Required field')
+    email: Yup.string()
+      .email('Invalid email format')
+      .required('Required'),
+    password: Yup.string().required('Required'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), ''], 'Passwords must match')
+      .required('Required'),
+    modeOfContact: Yup.string().required('Required'),
+    phone: Yup.string().when('modeOfContact', {
+      is: 'telephonemoc',
+      then: Yup.string().required('Required')
     })
   })
 
-  const handelSubmit = (value)=>{
-    console.log(value);
+  const onSubmit = values => {
+    console.log('Form data', values)
   }
+
   return (
-    <Formik initialValues={initialValues} validationSchema={validateYupSchema} handelSubmit={handelSubmit} >
-     {
-       formik => {
-         return (
-           <Form>
-             <FormikControl
-               control='input'
-               type='email'
-               label='Email Address'
-               name='email'
-             />
-              <FormikControl
-                 control='input'
-                 type='password'
-                 label='Password'
-                 name='password'
-               />
-                <FormikControl
-                   control='input'
-                   type='password'
-                   label='Confirm Password'
-                   name='confirmPassword'
-                 />
-                  <FormikControl
-                     control='radio'
-                    //  type='email'
-                     label='Mode of contact'
-                     name='modeOfContact'
-                     options={options}
-                   />
-                    <FormikControl
-                       control='input'
-                       type='email'
-                       label='Phone Number'
-                       name='phone'
-                     />
-           </Form>
-         )
-       }
-     }
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {formik => {
+        return (
+          <Form>
+            <FormikControl
+              control='input'
+              type='email'
+              label='Email'
+              name='email'
+            />
+            <FormikControl
+              control='input'
+              type='password'
+              label='Password'
+              name='password'
+            />
+            <FormikControl
+              control='input'
+              type='password'
+              label='Confirm Password'
+              name='confirmPassword'
+            />
+            <FormikControl
+              control='radio'
+              label='Mode of contact'
+              name='modeOfContact'
+              options={options}
+            />
+            <FormikControl
+              control='input'
+              type='text'
+              label='Phone number'
+              name='phone'
+            />
+            <button type='submit' disabled={!formik.isValid}>
+              Submit
+            </button>
+          </Form>
+        )
+      }}
     </Formik>
   )
 }
