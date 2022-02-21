0a75ca7f7f3f5dd8f46779d3fedb781ff383a12c
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-02-21 01:08:30 +0100

    getting credentials for the backend

--- PASSPORT/Server/passport.js
+++ PASSPORT/Server/passport.js
@@ -2,10 +2,11 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const GithubStrategy = require("passport-github2").Strategy;
 const FacebookStrategy = require("passport-facebook").Strategy;
 const passport = require("passport");
+// const env = require('')
 
 const GOOGLE_CLIENT_ID =
-  "your id";
-const GOOGLE_CLIENT_SECRET = "your id";
+  "28149128211-2bovbchm5cg143pe0asvmjk2vnvase1b.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
 
 GITHUB_CLIENT_ID = "your id";
 GITHUB_CLIENT_SECRET = "your id";
