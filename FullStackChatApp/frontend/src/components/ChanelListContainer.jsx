78490367d890f0633cdfa4eab8a9987e63ae0fef
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-02-28 13:37:08 +0100

    added something to gitignore

--- FullStackChatApp/frontend/src/components/ChanelListContainer.jsx
+++ FullStackChatApp/frontend/src/components/ChanelListContainer.jsx
@@ -1,9 +1,24 @@
 
 import React from 'react'
+import { ChannelList } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+
+export const Sidebar = ()=>{
+  <div className="channel-list__sidebar">
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__inner">
+        
+      </div>
+    </div>
+  </div>
+}
 
 const ChanelListContainer = () => {
   return (
-    <div></div>
+    <div>
+
+    </div>
   )
 }
 
