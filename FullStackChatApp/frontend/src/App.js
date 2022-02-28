7537ddd9dd0f0cc7108c7450aaedcdedc75c6b91
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-02-28 11:18:29 +0100

    installing packages

--- FullStackChatApp/frontend/src/App.js
+++ FullStackChatApp/frontend/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {StreamChat} from 'stream-chat'
+import {Chat} from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+const apiKey = 'xxw8udgwaycc'
+const client = StreamChat.getInstance(apiKey)
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="app__wrapper">
+      <Chat client={client} >
+        <ChanelListContainer 
+
+        />
+        <ChanelContainer />
+      </Chat>
     </div>
   );
 }
