2b40186bf5bb88c1df979e8e785358972b0c32ad
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-03-08 10:15:38 +0100

    actions and reducer to buy a cake

--- REDUX/src/App.js
+++ REDUX/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import CakeContainer from './components/Cakecontainer'
 import {Provider} from 'react-redux'
 import store from './redux/store';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 function App() {
   return (
     <Provider store={store} >
 
       <div className="App">
+        <HooksCakeContainer/>
         <CakeContainer/>
       </div>
     </Provider>
