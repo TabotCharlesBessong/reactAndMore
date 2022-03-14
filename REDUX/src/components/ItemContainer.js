7d4aa7b37fda633d8bae5ff91bb71f75b150ad8b
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-03-14 06:03:38 +0100

    mapDispatch to props

--- REDUX/src/components/ItemContainer.js
+++ REDUX/src/components/ItemContainer.js
@@ -2,16 +2,18 @@
 // import { connect } from 'formik'
 import React from 'react'
 import { connect } from 'react-redux'
+import { buyCake, buyCookies, buyIce } from '../redux'
 
 const ItemContainer = (props) => {
   return (
     <div>
       <h2>Item - {props.item}  </h2>
+      <button onClick={props.buyItem} > Buy Items </button>
     </div>
   )
 }
 
-const mapDispatchToProps  = (state , ownProps )=>{
+const mapStateToProps  = (state , ownProps )=>{
   const itemState = ownProps.cake ? state.cake.numOfCakes : ownProps.ice ? state.ice.numOfIceCreame : state.cookies.numOfCookies
 
   return {
@@ -19,4 +21,12 @@ const mapDispatchToProps  = (state , ownProps )=>{
   }
 }
 
-export default connect(mapDispatchToProps)(ItemContainer)
+const mapDispatchToProps = (dispatch , ownProps) =>{
+  const dispatchFunction = ownProps.cake ? () => dispatch(buyCake()) : ownProps.ice ? () => dispatch(buyIce()) : () => dispatch(buyCookies())
+
+  return {
+    buyItem  : dispatchFunction
+  }
+}
+
+export default connect(mapDispatchToProps ,mapDispatchToProps)(ItemContainer)
