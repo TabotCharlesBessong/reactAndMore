d1fed0b15c2005e9a5878018592b3f8b5be440a7
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-07-02 06:48:31 +0100

    added v6 protected routes

--- react-protected-routes-v6/starter/src/index.js
+++ react-protected-routes-v6/starter/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {AuthProvider} from './context/AuthProvider'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <AuthProvider>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
