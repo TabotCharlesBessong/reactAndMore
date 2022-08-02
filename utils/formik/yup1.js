f341810c073c29464c2de7b599d7d3b364acef62
Charles Bessong Tabot <82298650+TabotCharlesBessong@users.noreply.github.com>
2022-08-02 05:59:04 +0100

    Create yup1.js

---
+++ utils/formik/yup1.js
@@ -0,0 +1,17 @@
+const validate = Yup.object({
+    firstName: Yup.string()
+      .max(15, 'Must be 15 characters or less')
+      .required('Required'),
+    lastName: Yup.string()
+      .max(20, 'Must be 20 characters or less')
+      .required('Required'),
+    email: Yup.string()
+      .email('Email is invalid')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 charaters')
+      .required('Password is required'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Password must match')
+      .required('Confirm password is required'),
+  })
