0d504488487537f854e33fbcafcc62f7e0e18ed1
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-24 16:19:24 +0100

    from bad to worst

--- auth-protected-routes/src/main.jsx
+++ auth-protected-routes/src/main.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./hooks/auth";
+import { AuthProvider } from "./context/AuthProvider";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<AuthProvider>
-				<App />
+				<Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
 			</AuthProvider>
 		</BrowserRouter>
 	</React.StrictMode>
